591e8f248a0e5afc927368891cf37abccef41d6c
peiyingc <147575884+peiyingc@users.noreply.github.com>
2023-11-29 01:54:08 -0600

    Add files via upload

--- shaders/draw-scene.js
+++ shaders/draw-scene.js
@@ -41,7 +41,6 @@ function drawScene(gl, programInfo, buffers) {
     modelViewMatrix, // destination matrix
     modelViewMatrix, // matrix to rotate
     3.14, // amount to rotate in radians
-    [0, 0, 1],
   );
 
   // Tell WebGL how to pull out the positions from the position
