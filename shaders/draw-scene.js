22fef54e952d414383e5515decae1af42706ce22
peiyingc <147575884+peiyingc@users.noreply.github.com>
2023-11-29 03:48:56 -0600

    Add files via upload

--- shaders/draw-scene.js
+++ shaders/draw-scene.js
@@ -37,6 +37,12 @@ function drawScene(gl, programInfo, buffers) {
     [-0.0, 0.0, -6.0]
   ); // amount to translate
 
+  mat4.rotate(
+    modelViewMatrix, // destination matrix
+    modelViewMatrix, // matrix to translate
+    3.14
+  ); 
+
   // Tell WebGL how to pull out the positions from the position
   // buffer into the vertexPosition attribute.
   setPositionAttribute(gl, buffers, programInfo);
